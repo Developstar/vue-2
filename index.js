9e8b919fda25d7eb92757d460b84588b22aa0c04
Developstar <deyeye99@gmail.com>
2022-12-07 20:41:04 +0100

    Data and Method edited

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 new Vue({
     el:'#app',
     data: {
-        greetings: 'Hello guys',
-        firstName: '',
-        secondName: 'your firstName',
-        email : 'yourmail',
-        phone : 'your phone number',
+        greetings: `Hello guys, My name is `,
+        firstName: '...',
+        secondName: '...',
+        gmailIntro: 'You can reachout to me via',
+        email : '...',
+        comma : ',',
+        phone : '...',
         registration : 'Registration Form'
     },
 
     methods: {
         displayFirstName : function(e){
             this.firstName = e.target.value;
+        },
+
+        displaySecondName : function(e){
+            this.secondName = e.target.value;
+        },
+
+        displayEmail : function(e){
+            this.email = e.target.value;
+        },
+        
+        displayPhone : function(e){
+            this.phone = e.target.value;
         }
+        
+
+        
     }
 })
